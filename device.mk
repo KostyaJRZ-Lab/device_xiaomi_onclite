0c373feee0363171d6c712f53bd6b8873e2a4756
Michael Bestas <mkbestas@lineageos.org>
2021-08-25 09:51:07 +0530

    onclite: remove deprecated liboverlay target * This was removed from display HAL in 2015
    Signed-off-by: soumyajit007-creator <soumyajitpaul001@gmail.com>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
-    liboverlay \
     libqdMetaData \
     libqdMetaData.system \
     libgenlock \
