cf91aadf17a4154102466ed1ac281e78c6706b7d
KostyaJRZ <kostyajrzaws@gmail.com>
2021-10-08 01:01:38 +0300

    Revert "[TMP] onclite: Disable QCOM SEPolicy"
    This reverts commit 2d8af5f8b4f4a439bc0111c15fe09d89a35f4c80.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
-#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 #BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 SELINUX_IGNORE_NEVERALLOWS := true
