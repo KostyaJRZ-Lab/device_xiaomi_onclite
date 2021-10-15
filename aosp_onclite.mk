64a37852236a96f0b1c69080d451a349a685538e
KostyaJRZ <kostyajrzaws@gmail.com>
2021-10-15 15:06:36 +0300

    onclite: Include "vendor/ppui"

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PPUI stuff.
+$(call inherit-product, vendor/ppui/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_onclite
